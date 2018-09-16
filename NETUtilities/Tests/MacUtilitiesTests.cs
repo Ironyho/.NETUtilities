8c3fcd7ad23f52ec1ec2fb304df59341ca742347
Iron <iron.yehong@outlook.com>
2018-09-17 00:09:09 +0800

    Add unit test project

---
+++ NETUtilities/Tests/MacUtilitiesTests.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace Tests
+{
+    [TestClass]
+    public class MacUtilitiesTests
+    {
+        [TestMethod]
+        public void GetMacsByNetworkInterface()
+        {
+            var mac = Utilities.MacUtilities.GetMacsByNetworkInterface();
+            Assert.AreEqual(mac.Count, 1);  // on Iron's surface
+        }
+
+        [TestMethod]
+        public void GetMacsByWmi()
+        {
+            var macs = Utilities.MacUtilities.GetMacsByWmi();
+            Assert.AreEqual(macs.Count, 1); // on Iron's surface
+        }
+    }
+}
